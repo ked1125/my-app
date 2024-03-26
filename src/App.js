d11968a23941a40cd64ef7d8716039c22636c914
ked1125 <ked1125@naver.com>
2024-03-26 15:53:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  const han = "test입니다.";
+  return (
+    <div className="App">
+      <h3 className="title">{han}</h3>
+    </div>
+  );
+}
+
+export default App;
